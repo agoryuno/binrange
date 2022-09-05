16d7efc8da2633f81bcc4c34d90a173835c220e7
Alexander Goryunov <alex.goryunov@gmail.com>
2022-09-05 21:31:59 +0300

    Added walk() and type hints

--- src/binrange/binary_range.py
+++ src/binrange/binary_range.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import math
 from dataclasses import dataclass
-from typing import Union
+from typing import Union, Callable
 
 # We don't include complex numbers
 # in our Numeric data type for simplicity's
@@ -28,22 +28,30 @@ class ValueRange:
     def _range(self):
         return self.maxvalue - self.minvalue
     
-    def __eq__(self, other):
+    def __eq__(self, other: ValueRange):
         return self._range() == other._range()
     
-    def __lt__(self, other):
+    def __lt__(self, other: ValueRange):
         return self._range() < other._range()
     
-    def __le__(self, other):
+    def __le__(self, other: ValueRange):
         return self._range() <= other._range()
     
-    def __gt__(self, other):
+    def __gt__(self, other: ValueRange):
         return self._range() > other._range()
     
-    def __ge__(self, other):
+    def __ge__(self, other: ValueRange):
         return self._range() >= other._range()
 
-def build_tree(val_range, floor_range):
+def build_tree(val_range: ValueRange, floor_range: Numeric) -> ValueRange:
+    """
+    Builds a binary tree partition starting from the val_range
+    value range root. The floor_range set the minimum range upon
+    reaching which we form a leaf and abandon the current branch.
+
+    Returns a ValueRange object, representing a binary tree.
+    """
+
     minval, maxval = val_range.minvalue, val_range.maxvalue
     
     floors = ((maxval - minval) // 2) / floor_range
@@ -55,3 +63,19 @@ def build_tree(val_range, floor_range):
     val_range.right = build_tree(ValueRange(midval, maxval), floor_range)
     
     return val_range
+
+
+def walk(val_range: ValueRange, fun: Callable[[ValueRange], bool]):
+    """
+    Walks the binary tree starting at val_range ValueRange node.
+
+    The fun argument is a callable that can be used to terminate the walk
+    by returning False. Since fun receives the current node when being called
+    it can be used to check some conditions, filter nodes, etc, using side effects.
+    """
+    if not fun(val_range):
+        return
+    if val_range.left is not None:
+        walk(val_range.left, fun)
+    if val_range.right is not None:
+        walk(val_range.right, fun)
